756716e207185ac1bd46ba70300e63468e888e0d
classyoon <96508736+classyoon@users.noreply.github.com>
2025-02-14 15:48:41 -0500

    Refactored UI Prototype again to be more clean.

--- FiniYelpInternship/BusinessesView.swift
+++ FiniYelpInternship/BusinessesView.swift
@@ -9,16 +9,36 @@ import SwiftUI
 
 struct BusinessesView: View {
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack(spacing: 0){
-                BusinessCardView()
-                BusinessCardView()
-                BusinessCardView()
-            }
+        VStack{
+            cardsView
+            previousAndNextButtons
         }
     }
 }
-
+extension BusinessesView {
+    var cardsView : some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 0){
+                ForEach(0..<3, id: \.self) { _ in
+                    BusinessCardView()
+                        .containerRelativeFrame(.horizontal, alignment: .center)
+                        .scrollTransition(topLeading: .interactive, bottomTrailing: .interactive) { effect, phase in
+                            effect
+                                .scaleEffect(1 - abs(phase.value * 0.4))
+                            
+                        }
+                }
+            }
+        }
+        .scrollTargetBehavior(.paging)
+    }
+    var previousAndNextButtons : some View {
+        HStack {
+            Button("Prev"){}
+            Button("Next"){}
+        }.buttonStyle(.bordered)
+    }
+}
 #Preview {
     BusinessesView()
 }
