a8c159fa99c640ad86812d72b85347ebf09d4a1b
classyoon <96508736+classyoon@users.noreply.github.com>
2025-02-14 15:30:03 -0500

    Initial Commit

---
+++ FiniYelpInternship/FiniYelpInternshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiniYelpInternshipApp.swift
+//  FiniYelpInternship
+//
+//  Created by Conner Yoon on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct FiniYelpInternshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FiniYelpInternshipTests/FiniYelpInternshipTests.swift
@@ -0,0 +1,17 @@
+//
+//  FiniYelpInternshipTests.swift
+//  FiniYelpInternshipTests
+//
+//  Created by Conner Yoon on 2/14/25.
+//
+
+import Testing
+@testable import FiniYelpInternship
+
+struct FiniYelpInternshipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
